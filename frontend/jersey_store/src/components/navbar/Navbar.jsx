cf6a35d2b5453c9cc485787277fb12dac1bfc785
Ayeshmantha <ravindu.ayesh123@gmail.com>
2024-01-16 10:06:42 +0530

    AllLabel Completed with the responsive desing.

--- frontend/jersey_store/src/components/navbar/Navbar.jsx
+++ frontend/jersey_store/src/components/navbar/Navbar.jsx
@@ -1,23 +1,40 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './navbar.css';
 import logo from '../../assets/logo.png';
 import bag from '../../assets/bag.png';
 import heart from '../../assets/heart.png';
 import user from '../../assets/user.png';
 import search from '../../assets/search.png';
+import { RiMenu3Line, RiCloseLine } from 'react-icons/ri';
+
+
+const NavCategory = () => (
+  <>
+    <p><a href="#Men">MEN</a></p>
+    <p><a href="#Women">WOMEN</a></p>
+    <p><a href="#Kids">KIDS</a></p>
+    <p><a href="#All">ALL</a></p>
+  </>
+);
+
+
+
 const Navbar = () => {
+
+  const [toggleMenu, setToggleMenu] = useState(false)
+
   return (
 
     <div className='dw__navbar'>
       <div className='dw__navbar_topbar'>
         <div className='dw__navbar_topbar_links'>
-        <img src={user} alt="" />
-        <div/>
-        <p><a href="">Help</a></p>
-        <div/>
-        <p><a href="">Register</a></p>
-        <div/>
-        <p><a href="">Contact Us</a></p>
+          <img src={user} alt="" />
+          <div />
+          <p><a href="">Help</a></p>
+          <div />
+          <p><a href="">Register</a></p>
+          <div />
+          <p><a href="">Contact Us</a></p>
         </div>
       </div>
 
@@ -28,26 +45,42 @@ const Navbar = () => {
           </div >
 
           <div className='dw__navabar_midbar_links_buttons'>
-            <p><a href="#Men">MEN</a></p>
-            <p><a href="#Women">WOMEN</a></p>
-            <p><a href="#Kids">KIDS</a></p>
-            <p><a href="#All">ALL</a></p>
+            <NavCategory />
           </div>
 
           <div className='dw__navbar_midbar_links_right'>
             <div className='dw__navbar_midbar_links_right__search-bar'>
-       
-                <img src={search} alt="search" />
-             
+
+              <img src={search} alt="search" />
+
               <div>
                 <input type="text" placeholder='Search for a produt' />
               </div>
 
             </div>
+
             <div className='dw__navbar_midbar_links_right__icons'>
               <img src={bag} alt="bag" />
               <img src={heart} alt="heart" />
               <img src={user} alt="user" />
+            </div>
+
+            <div className='dw_nabar-menu'>
+              {toggleMenu
+                ? <RiCloseLine color="#000000" size={27} onClick={() => setToggleMenu(false)} />
+                : <RiMenu3Line color="#000000" size={27} onClick={() => setToggleMenu(true)} />}
+              {toggleMenu && (
+                <div className='dw_navabar-menu_container scale-up-top'>
+                  <div className='dw_navabar-menu_container-category'>
+                    <NavCategory />
+                    <div className='dw_navabar-menu_container-icons'>
+                      <img src={bag} alt="bag" />
+                      <img src={heart} alt="heart" />
+                      <img src={user} alt="user" />
+                    </div>
+                  </div>
+                </div>
+              )}
             </div>
 
           </div>
--- frontend/jersey_store/src/pages/home/label_all/LabelAll.jsx
+++ frontend/jersey_store/src/pages/home/label_all/LabelAll.jsx
@@ -1,9 +1,22 @@
-import React from 'react'
-import './labelAll.css'
+import React from 'react';
+import './labelAll.css';
+import leftImage from '../../../assets/labelAll_left.png';
+import righttImage from '../../../assets/labelAll_right.png';
 
 const LabelAll = () => {
   return (
-    <div>LabelAll</div>
+    <div className='dw__allLabel'>
+      <img src={leftImage} alt="img_men" className='dw__allLabel_leftImg' />
+
+      <div className='dw__allLabel_content'>
+          <h1>NEW YEAR NEW JERSEYS</h1>
+          <p>New Jerseys are coming  and they are so good </p>
+          <div className='dw__allLabel_content-button'>SHOP NOW</div>
+      </div>
+
+      <img src={righttImage} alt="img_women" className='dw__allLabel_rightImg' />
+
+    </div>
   )
 }
 
